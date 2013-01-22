c54ec4c05e34d2c9f19f1ee6bcc9043d129ff862
Li Haoyi <haoyi.sg@gmail.com>
2013-01-22 03:37:31 -0500

    Stuff works

--- src/main/scala/websockets/Main.scala
+++ src/main/scala/websockets/Main.scala
@@ -2,14 +2,20 @@ package websockets
 
 
 import scala.concurrent.duration._
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{ActorRef, Props, ActorSystem}
 import akka.pattern.ask
 import spray.util._
 import spray.io._
 import java.net.InetSocketAddress
-import spray.io.IOBridge.Key
-import java.nio.{ByteBuffer, CharBuffer}
+import spray.io.IOBridge.{Connection, Key}
+import java.nio.ByteBuffer
 import java.security.MessageDigest
+import spray.can.server.HttpServer
+import spray.can.server.StatsSupport.StatsHolder
+import spray.http.{HttpResponse, HttpRequest}
+
+import scala.Some
+import spray.http.HttpResponse
 
 object Main  {
   def main(args: Array[String]){
@@ -20,23 +26,25 @@ object Main  {
     server.ask(IOServer.Bind("localhost", 80))(1 second span)
           .onSuccess { case IOServer.Bound(endpoint, _) => println("\nBound echo-server to " + endpoint) }
   }
+
 }
+
 
 class EchoServer extends IOServer {
   val ioBridge = IOExtension(context.system).ioBridge()
 
   override def bound(endpoint: InetSocketAddress, bindingKey: Key, bindingTag: Any): Receive =
     super.bound(endpoint, bindingKey, bindingTag) orElse {
-
-      case IOBridge.Received(handle, buffer) =>
-
+      case IOBridge.Received(connection, buffer) =>
         try {
+
           val headers = buffer.array.asString.trim.lines.drop(1).map(_.split(": ")).map(x => x(0) -> x(1)).toMap
+
           val byteHash = MessageDigest.getInstance("SHA-1")
             .digest(
             (headers("Sec-WebSocket-Key") + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")
           )
-          println(byteHash.length)
+
           val returnValue =new sun.misc.BASE64Encoder().encode(byteHash)
 
           val b = "HTTP/1.1 101 Switching Protocols\r\n" +
@@ -44,17 +52,13 @@ class EchoServer extends IOServer {
                   "Connection: Upgrade\r\n" +
                   s"Sec-WebSocket-Accept: $returnValue\r\n\r\n"
 
-          ioBridge ! IOBridge.Send(handle, ByteBuffer.wrap(b.getBytes), Some('SentOk))
-        }catch{case _: Exception =>
+          ioBridge ! IOBridge.Send(connection, ByteBuffer.wrap(b.getBytes), Some('SentOk))
+        }catch{ case _: Exception =>
           println("Exception!")
           val f = model.Frame.make(buffer.array)
-          val newF = f.copy(maskingKey = None)
-          ioBridge ! IOBridge.Send(handle, ByteBuffer.wrap(newF.write), Some('SentOk))
+          val newF = f.copy(maskingKey = None, data = f.data.toUpperCase())
+          ioBridge ! IOBridge.Send(connection, ByteBuffer.wrap(newF.write), Some('SentOk))
         }
-
-
-      case 'SentOk =>
-        println("Send completed")
 
       case IOBridge.Closed(_, reason) =>
         println("Connection closed: {}", reason)
