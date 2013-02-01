aee49884787419c6fe70efb8ad8eebefb9c2c800
Li Haoyi <haoyi.sg@gmail.com>
2013-02-01 14:31:28 -0500

    Updated to latest nightly

--- project/Build.scala
+++ project/Build.scala
@@ -17,12 +17,12 @@ object Build extends sbt.Build{
         "spray nightly"      at "http://nightlies.spray.io/"
       ),
       libraryDependencies ++= Seq(
-        "io.spray"                %   "spray-can"     % "1.1-20130108",
+        "io.spray"                %   "spray-can"     % "1.1-20130129",
         "com.typesafe.akka"       %%  "akka-actor"    % "2.1.0",
         "com.typesafe.akka"       %%  "akka-testkit"    % "2.1.0" % "test",
         "io.spray"                %%  "spray-json"    % "1.2.3",
-        "io.spray"                %  "spray-httpx"    % "1.1-20130108",
-        "io.spray"                %  "spray-client"    % "1.1-20130108",
+        "io.spray"                %  "spray-httpx"    % "1.1-20130129",
+        "io.spray"                %  "spray-client"    % "1.1-20130129",
         "org.scalatest" % "scalatest_2.10.0" % "2.0.M5" % "test",
         "org.seleniumhq.selenium" % "selenium-java" % "2.28.0" % "test",
         "org.scalacheck" %% "scalacheck" % "1.10.0" % "test"
